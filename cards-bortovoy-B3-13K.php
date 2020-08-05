a4958d378abefdc44421af31c76ed1d18fffdfe4
fasRuslan <faskhutdinoff.rus@yandex.ru>
2020-08-05 14:37:52 +0300

    поменял футер

--- cards-bortovoy-B3-13K.php
+++ cards-bortovoy-B3-13K.php
@@ -171,46 +171,9 @@
 				</div>	
 			</div>
 		</section>
-	<footer class="contacts">
-		<div class="adress">
-			<h2>ООО «ТНТ»</h2>
-			<p>+7(921)937-20-00</p>
-			<p>+7(812)414-97-11</p>
-			<b>Наш адрес</b>
-			<p>196626, г.Санкт-Петербург</p>
-			<p>пос.Шушуры, улица Ленина д. 1 Литера И</p>
-		</div>
-		<div class="office-hours">
-			<b>Часы работы</b>
-			<ul class="oficce-list">
-				<li class="oficce-item">Понедельник — Пятница с 9-00 до 18-00</li>
-				<li class="oficce-item">сб-вс: выходные дни</li>
-			</ul>
-		</div>
-		<div class="semitrailer">
-			<b>Полуприцепы</b>
-			<ul class="semilitraiter-list">
-				<li class="semilitrailer-item"><a href="s-conteinerovoz-polupricep.php">Контейнеровозы</a></li>
-				<li class="semilitrailer-item"><a href="s-sortimentovoz-polupricep.php">Сортиментовозы</a></li>
-				<li class="semilitrailer-item"><a href="s-samosvalynaya-texnika.php">Самосвальная техника</a></li>
-				<li class="semilitrailer-item"><a href="s-tentovanniy-polupricep.php">Тентованные</a></li>
-				<li class="semilitrailer-item"><a href="s-bortovoy-polupricep.php">Бортовые</a></li>
-				<li class="semilitrailer-item"><a href="s-sdignii-poli.php">Со сдвижными полами</a></li>
-				<li class="semilitrailer-item"><a href="s-scotovozi.php">Скотовозы</a></li>
-				<li class="semilitrailer-item"><a href="s-isotherm-polupricep.php">Изотермические</a></li>
-				<li class="semilitrailer-item"><a href="s-pticevozi.php">Птицевозы</a></li>
-				<li class="semilitrailer-item"><a href="s-zernovye.php">Зерновозы</a></li>
-				<li class="semilitrailer-item"><a href="s-osi.php">Оси для полуприцепов</a></li>
-			</ul>
-		</div>
-		<div class="information">
-			<b>Информация</b>
-			<ul class="information-list">
-				<li class="information-item"><a href="#">О компании</a></li>
-				<li class="information-item"><a href="#">Лизинг</a></li>
-			</ul>
-		</div>
-	</footer>
+		<?php
+	include 'footer.php'
+	?>
 	<script src="js/main.js"></script>
 	<script src="js/form.js"></script>
 </body>
