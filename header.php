bb7683ec44bf7b0817ea9ee8ff1d091e4983f730
fasRuslan <faskhutdinoff.rus@yandex.ru>
2020-08-05 14:07:53 +0300

    начинаем делать хедер

---
+++ header.php
@@ -0,0 +1,34 @@
+<header>
+		<div class="header-info">
+			<div class=" tonar-first">
+				<h1>Машиностроительный завод Тонар.</h1>
+				<b>Основан в 1990 году</b>
+			</div>
+			<a href="tel:+7 (921) 937-2000">+7 (921) 937-2000</a>
+		</div>
+		<div class="wrapper">
+			<div class="header-logo">
+				<img src="img/general-page/logo1.jpeg" alt="">
+				<img src="img/general-page/zavod.jpeg" alt="">
+				<img src="img/general-page/logo2.jpeg" alt="">
+			</div>
+			<nav class="header-navigation">
+				<ul class="navigation-list">
+					<li class="navigation-item"><a href="#">О компании</a></li>
+					<li class="navigation-item"><a href="#">Полуприцепы</a></li>
+					<li class="navigation-item"><a href="#">Запчасти</a></li>
+					<li class="navigation-item"><a href="#">Лизинг</a></li>
+					<li class="navigation-item"><a href="#">Контакты</a></li>
+					<button class="call-form">Заказать звонок</button>
+				</ul>
+			</nav>
+
+			<form action="mail.php" method="POST" class="form">
+				<p class="form-title">Введите свой номер телефона</p>
+				<input type="tel" name="tel" required>
+				<input type="submit" value="Отправить" class="btnSend">
+				<input type="submit" value="Отмена" class="btnCancel">
+			</form>
+		</div>
+	</header>
+	
